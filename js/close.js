6bcb5519b1cffb024bdb9a530944a7f00e71c9b5
ErykLoom <e.wroblewski@loom.de>
2018-11-16 17:33:26 +0100

    last push

--- js/close.js
+++ js/close.js
@@ -1,14 +1,14 @@
 (function($) {
 
-  var closeX = $('#footer-close-x');
-  var closeBtn = $('#footer-close-btn');
+  var closeX = $('.footer-close-x');
+  var closeBtn = $('.form__submit-sticky');
 
   closeX
     .click(function() {
       $('.footer-sticky').css({
         'display': 'none',
       })
-  });
+    });
 
   closeBtn
     .click(function() {
